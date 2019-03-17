865ed4df37e5ff02ff8256a171462bc303f6cc53
Piotr Telman <piotr.telman@gmail.com>
2019-03-17 17:31:47 +0100

    Added basic models

---
+++ PutCoin.Model/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PutCoin.Model
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public IEnumerable<Guid> OriginTransactionIds { get; set; }
+        public IEnumerable<TransactionDestination> Destinations { get; set; }
+        public string Signature { get; set; }
+    }
+}
---
+++ PutCoin.Model/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PutCoin.Model
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+    }
+}
